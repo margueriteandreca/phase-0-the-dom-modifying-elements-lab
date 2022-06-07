32a664a139f2849cbcdc6be7ffad56d9390620a6
+ <+>
2022-06-07 16:19:02 -0400

    commit tree 1fbc923907581409b1429b7ce4327a313b9c9fc8

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.querySelector("#main").remove()
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "Marguerite is the champion"
+document.body.append(newHeader)
+
